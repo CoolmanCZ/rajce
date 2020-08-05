af529551c9311b7973e373623b264f0724be50d3
CoolmanCZ <coolman@centrum.cz>
2020-08-05 20:06:51 +0200

    Submodules update

--- GenerateCMakeFiles.sh
+++ GenerateCMakeFiles.sh
@@ -23,12 +23,26 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+OS=`uname -a | cut -d ' ' -f 1`
+if [ "${OS}" == "Linux" ]; then
+  START=$(date +%s.%N)
+fi
 
 source upp_cmake/GenerateCMakeFiles-lib.sh
 
-GENERATE_VERBOSE="1"
-GENERATE_DEBUG="0"
-GENERATE_PACKAGE="1"
+GENERATE_VERBOSE="1"        # set to "1" - enable additional output during script processing on the screen
+GENERATE_DEBUG="0"          # set to "1" - enable debug output during script processing on the screen
+GENERATE_PACKAGE="1"        # set to "1" - create a tarball package of the project
+
+#GENERATE_NOT_Cxx="1"                # set to "1" - do not use compiler -std=c++14 parameter (compiler parameter is enabled as default)
+#GENERATE_NOT_PARALLEL="1"           # set to "1" - do not build with multiple processes (parralel build is enabled as default)
+#GENERATE_NOT_PCH="1"                # set to "1" - do not build with precompiled header support (precompiled header support is enabled as default)
+if [ "${OS}" == "SunOS" ]; then
+  GENERATE_NOT_REMOVE_UNUSED_CODE="1" # set to "1" - do not use compile and link parameters to remove unused code and functions (unused code and functions are removed as default)
+fi
+
+CMAKE_VERBOSE_OVERWRITE="0" # set to "0" - do not generate cmake verbose makefile output (even when the debug flag is set)
+#CMAKE_VERBOSE_OVERWRITE="1" # set to "1" - always generate cmake verbose makefile output
 
 UPP_SRC_BASE="ultimatepp"
 UPP_SRC_DIR="${UPP_SRC_BASE}/uppsrc"
@@ -39,12 +53,17 @@ PROJECT_EXTRA_COMPILE_FLAGS=""
 PROJECT_EXTRA_LINK_FLAGS=""
 
 if [ $# -lt 1 ]; then
-  echo "# linux build"
+  echo "# POSIX build"
   PROJECT_FLAGS="-DflagGUI -DflagMT -DflagGCC -DflagLINUX -DflagPOSIX -DflagSHARED"
 else
-  echo "# MinGW build"
+  echo "# WINDOWS build"
   PROJECT_FLAGS="-DflagGUI -DflagMT -DflagGCC -DflagPOSIX"
 fi
 
 generate_main_cmake_file "${PROJECT_NAME}" "${PROJECT_FLAGS}"
+
+if [ "${OS}" == "Linux" ]; then
+  DUR=$(echo "$(date +%s.%N) - ${START}" | bc)
+  echo "Execution time: $(date -d@0${DUR} -u +%H:%M:%S.%N)"
+fi
 
--- utils-FormatSource.sh
+++ utils-FormatSource.sh
@@ -8,6 +8,6 @@ for i in ${files}; do
 		continue;
 	fi
 	#echo "${i}"
-	clang-format -style="{BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Always, SortIncludes: false, ColumnLimit: 100}" -i "${i}"
+	clang-format -style="file" -i "${i}"
 done
 
